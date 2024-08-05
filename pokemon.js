9418bf489964bc929c9e023dd2c71017e882b462
solesen1992 <saramundal@live.dk>
2024-08-05 16:55:51 +0200

    Update Pokemon.js - Getting nameFilter working

--- pokemon.js
+++ pokemon.js
@@ -1,4 +1,4 @@
-const MAX_POKEMON = 151; //maximum amount of pokemon we're gonna retrieve from the pokemon API
+const MAX_POKEMON = 649; //maximum amount of pokemon we're gonna retrieve from the pokemon API
 const listWrapper = document.querySelector(".list-wrapper"); //referencing the HTML elements. The place we insert pokemon.
 const searchInput = document.querySelector("#search-input");
 const numberFilter = document.querySelector("#number");
@@ -98,7 +98,7 @@ function handleSearch() {
         });
     } else if (nameFilter.checked) {
         filteredPokemons = allPokemons.filter((pokemon) => {
-            pokemon.name.toLowerCase().startsWith(searchTerm)
+            return pokemon.name.toLowerCase().startsWith(searchTerm)
     });
     } else {
         filteredPokemons = allPokemons;
